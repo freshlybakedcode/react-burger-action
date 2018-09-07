674a3e4686e1c21fd344f58f32e5798ec7f45098
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2018-09-07 13:18:07 +0100

    Performance improvement - only update when visible

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.css'
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-  return (
-    <React.Fragment>
-      <Backdrop 
-        show={props.show} 
-        click={props.closeModal}
-      />
-      <div 
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}
-      >
-        {props.children}
-      </div>
-    </React.Fragment>
-  )
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  componentWillUpdate() {
+    console.log('[modal.js] will update');
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop 
+          show={this.props.show} 
+          click={this.props.closeModal}
+        />
+        <div 
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default Modal
